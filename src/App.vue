f958d7b1115da49c707cbfc6796929693c7f2512
Dylan Whitney <dwhitne@rei.com>
2021-04-01 17:00:56 -0500

    Basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <time-table msg="A timeline of the late bronze age"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimeTable from './components/TimeTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TimeTable
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import url('./style/global.scss');
 </style>
---
+++ src/components/TimeTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="cdr-container">
+    <cdr-text tag="h1" modifier="heading-serif-900">{{ msg }}</cdr-text>
+
+    <cdr-table border>
+      <thead>
+        <tr>
+          <th id="stripedhead1" scope="col">Year (B.C.E.)</th>
+          <th id="stripedhead2" scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="thing in things" :key="thing.year">
+          <td headers="stripedhead1">{{ thing.year }}</td>
+          <td headers="stripedhead2">
+            <div class="events" v-if="thing.events.length">
+              <div class="event" v-for="(stuff, tags, index) in thing.events" :key="index">
+                {{ stuff.event }}
+              </div>
+            </div>
+            
+          </td>
+        </tr>
+      </tbody>
+    </cdr-table>
+  </div>
+</template>
+
+<script>
+import { CdrTable, CdrText } from '@rei/cedar';
+import things from './events';
+
+export default {
+  name: 'TimeTable',
+  props: {
+    msg: String
+  },
+  components: {
+    CdrTable,
+    CdrText,
+  },
+  data() {
+    return {
+      things
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event:not(:first-child) {
+  margin-top: 0.8rem;
+}
+</style>
